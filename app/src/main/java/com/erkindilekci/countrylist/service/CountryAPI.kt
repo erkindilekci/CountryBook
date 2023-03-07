3df13f756f3a74d75b79f8a17f422e1d1b8c3e26
Erkin Dilekci <dilekcierkin@gmail.com>
2023-03-07 12:33:48 +0300

    Revert "CountryList App"
    This reverts commit e3e9f731df404daad965d040138daae0c0564bf6.

--- app/src/main/java/com/erkindilekci/countrylist/service/CountryAPI.kt
+++ app/src/main/java/com/erkindilekci/countrylist/service/CountryAPI.kt
@@ -5,7 +5,10 @@ import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface CountryAPI {
-    @GET("erkindil/Json/main/cryptolist.json")
+    // https://raw.githubusercontent.com/
+    // atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json
+
+    @GET("atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json")
     fun getCountries(): Single<List<Country>>
 }
 
